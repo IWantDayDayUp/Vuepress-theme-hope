86cc987464c29dccee748aa5ae86bef6a3b4d350
再也不敢 <1462959616@qq.com>
2023-10-25 13:04:43 +0800

    🦄 refactor: Update the navigation of my blog

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,51 +2,47 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   "/",
-  // "/demo/",
-  // {
-  //   text: "Posts",
-  //   icon: "pen-to-square",
-  //   prefix: "/posts/",
-  //   children: [
-  //     {
-  //       text: "Apple",
-  //       icon: "pen-to-square",
-  //       prefix: "apple/",
-  //       children: [
-  //         { text: "Apple1", icon: "pen-to-square", link: "1" },
-  //         { text: "Apple2", icon: "pen-to-square", link: "2" },
-  //         "3",
-  //         "4",
-  //       ],
-  //     },
-  //     {
-  //       text: "Banana",
-  //       icon: "pen-to-square",
-  //       prefix: "banana/",
-  //       children: [
-  //         {
-  //           text: "Banana 1",
-  //           icon: "pen-to-square",
-  //           link: "1",
-  //         },
-  //         {
-  //           text: "Banana 2",
-  //           icon: "pen-to-square",
-  //           link: "2",
-  //         },
-  //         "3",
-  //         "4",
-  //       ],
-  //     },
-  //     { text: "Cherry", icon: "pen-to-square", link: "cherry" },
-  //     { text: "Dragon Fruit", icon: "pen-to-square", link: "dragonfruit" },
-  //     "tomato",
-  //     "strawberry",
-  //   ],
-  // },
-  // {
-  //   text: "V2 Docs",
-  //   icon: "book",
-  //   link: "https://theme-hope.vuejs.press/",
-  // },
+  "/demo/",
+  {
+    text: "Algorithm",
+    icon: "pen-to-square",
+    prefix: "/posts/Algorithm/",
+    children: [
+      {
+        text: "Codeforces",
+        icon: "pen-to-square",
+        prefix: "Codeforces/",
+        children: [
+          // { text: "Apple1", icon: "pen-to-square", link: "1" },
+          // { text: "Apple2", icon: "pen-to-square", link: "2" },
+          // { text: "项目骨架", link: "https://github.com/macrozheng/mall-tiny",},
+          // "3",
+          // "4",
+
+        ],
+      },
+      {
+        text: "AtCoder",
+        icon: "pen-to-square",
+        prefix: "AtCoder/",
+        children: [
+          // { text: "Apple1", icon: "pen-to-square", link: "1" },
+          // { text: "Apple2", icon: "pen-to-square", link: "2" },
+          // "3",
+          // "4",
+        ],
+      },
+      {
+        text: "LeetCode",
+        icon: "pen-to-square",
+        prefix: "LeetCode/",
+        children: [
+          // { text: "Apple1", icon: "pen-to-square", link: "1" },
+          // { text: "Apple2", icon: "pen-to-square", link: "2" },
+          // "3",
+          // "4",
+        ],
+      },
+    ],
+  },
 ]);
